abffe81b56df0d9974a52401aa5ebc646345546f
OmegaRus <omega@nimas.li>
2017-07-24 13:18:53 +0300

    changes

--- example.sh
+++ example.sh
@@ -1,1 +1,8 @@
-mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.1:get -DrepoUrl=https://repo.maven.apache.org/maven2 -Dartifact=org.eclipse.persistence:eclipselink:2.6.1 -Dtransitive=true
+#!/bin/bash
+set -e
+rm -rf ~/.m2/repository
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.1:get \
+    -DrepoUrl=https://repo.maven.apache.org/maven2 \
+    -Dartifact=org.eclipse.persistence:eclipselink:2.6.1 \
+    -Dtransitive=true
+tar czvf artifacts.tar.gz -C ~/.m2/repository .
