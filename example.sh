5f70196d5f1adde63db964b3b488843a31c9421a
OmegaRus <omega@nimas.li>
2017-08-03 11:02:37 +0300

    update

--- example.sh
+++ example.sh
@@ -2,7 +2,7 @@
 set -e
 rm -rf ~/.m2/repository
 mvn org.apache.maven.plugins:maven-dependency-plugin:3.0.1:get \
-    -DrepoUrl=https://repo.maven.apache.org/maven2 \
-    -Dartifact=org.eclipse.persistence:eclipselink:2.6.1 \
+    -DremoteRepositories=https://plugins.gradle.org/m2 \
+    -Dartifact=gradle.plugin.org.jetbrains.intellij.plugins:gradle-intellij-plugin:0.2.13 \
     -Dtransitive=true
 tar czvf artifacts.tar.gz -C ~/.m2/repository .
